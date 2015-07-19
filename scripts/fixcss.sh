a91c4680f46755959fdd43e159fbe42b4875e087
selby <selby.kendrick@gmail.com>
2015-07-18 20:29:13 -0500

    some bash scripts to make life easier

---
+++ scripts/fixcss.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ./fixcss.sh
+# Usage: ./fixcss.sh <file.css> <keep backup: 1 or 0>
+
+# Lets fix some CSS
+cat $1 | grep --quiet -i "\\\9" && {
+  echo "CSS invalid, attempting to fix"
+  rm $1.bak -f
+  echo "Creating backup..."
+  mv $1 $1.bak
+  echo "Replacing invalid string..."
+  cat $1.bak | sed -r s/\\\\9//g | tee $1 1> /dev/null
+  cat $1 | grep --quiet -i "\\\9" && {
+    echo "CSS still invalid, sorry =/"
+    echo "Replacing original file..."
+    rm $1 -f
+    mv $1.bak $1
+  } || echo "Good to go =)"
+  $2 || echo "Removing backup..." && rm $1.bak -f
+} || echo "Not broken, don't worry about it"
---
+++ scripts/gendocs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ./gendocs.sh
+# Usage: ./gendocs.sh <swagger.json> <target_dir>
+
+# Gets directory script is running from
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# Generate Docs
+bootprint swagger $1 $2
+# Fix invalid CSS
+$DIR/fixcss.sh $2/main.css
